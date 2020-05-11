368472608a213b1c40298cc0eaf348c4eb0132e2
Ryan <butryan@amazon.com>
2020-05-11 08:07:18 +0200

    Updated licenses, README. Minor changes to options.

--- README.md
+++ README.md
@@ -1,37 +1,38 @@
 # getitdone
 
 ### About
-Command line to-do list application.
+    Command line to-do list application.
 
-I was browsing on the Internet and wanted a really basic todo list application, which was free and opensource. Since I am in the process of learning more about software development I decided to create my own.
+    I was browsing on the Internet and wanted a really basic todo list application, which was free and opensource. Since I am in the process of learning more about software development I decided to create my own.
 
 
 ### Installation
-$ pip install getitdone
+`$ pip install getitdone`
 
 ### Uninstall
-$ pip uninstall getitdone
+`$ pip uninstall getitdone`
 
 ### User Manual
-```
-OPTIONS
-    -new or -n '<task-name>'
-        Create a new task with name in first argument
-    -update or -u '<task-name>' '<new-name>'
-        Update task in first argument with value second argument
-    -delete or -d '<task-name>'
-        Delete task in first argument with value second argument
-    -show or -s '<task-name>'
-        Delete task in first argument with value second argument
-    -help or -h
-        Shows man page for todolist
-```
+    OPTIONS
+        --new or -n '<task-name>'
+            Create a new task with name in first argument
+        --update or -u '<task-name>' '<new-name>'
+            Update task in first argument with value second argument
+        --delete or -d '<task-name>'
+            Delete task in first argument with value second argument
+        --show or -s '<task-name>'
+            Delete task in first argument with value second argument
+        --help or -h
+            Shows man page for todolist
 
 ### Report an issue
+    Create an issue on GitHub with as much supporting information and detail about the bug or issue as possible.
 
 ### Contribute
+    Anyone is welcome to contribute to the project. You can submit your pull request and the changes will be reviewed. When submitting changes, ensure your code is "Pythonic". Use only Stand Library modules. No module are allowed that need further installation.
 
 ### License
+    Licensing is AGPL by default but an MIT license is available for purchase.
 
 ### Credits
-Duanne Mattheus - Providing the name for the application. Thank you Duanne :)
+    Duanne Mattheus - Thank you fo providing the name for the application.
--- getitdone/src/getitdone/getitdone.py
+++ getitdone/src/getitdone/getitdone.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-"""
-`getitdone` to-do list application
-"""
+# `getitdone` to-do list application
+#
+#     Copyright (C) 2020  Ryan Butler
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as
+#     published by the Free Software Foundation, either version 3 of the
+#     License, or (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+#
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 
@@ -32,7 +45,6 @@ def new(task_name):
 
 
 def update(task_name, new_name):
-    # TODO: integrate updates with db
     user_input = input(
         f"Update '{task_name}' with '{new_name}'? Y/N "
     )
@@ -46,7 +58,6 @@ def update(task_name, new_name):
 
 
 def delete(task_name):
-    # TODO: integrate deletes with db
     user_input = input(f"Delete '{task_name}' from your list? Y/N ")
     if user_input.lower() == "y":
         query = f"DELETE FROM tasks WHERE name = '{task_name}'"
@@ -73,15 +84,15 @@ def command_help():
     print("\n### get-it-done ###\n")
     print(
         "OPTIONS\n"
-        "\t`-new` or `-n` '<task-name>'\n"
+        "\t`--new` or `-n` '<task-name>'\n"
         "\t\tCreate a new task with name in first argument\n\n"
-        "\t`-update` or `-u` '<task-name>' '<new-name>'\n"
+        "\t`--update` or `-u` '<task-name>' '<new-name>'\n"
         "\t\tUpdate task in first argument with value second argument\n\n"
-        "\t`-delete` or `-d` '<task-name>'\n"
+        "\t`--delete` or `-d` '<task-name>'\n"
         "\t\tDelete task in first argument with value second argument\n\n"
-        "\t`-show` or `-s` '<task-name>'\n"
+        "\t`--show` or `-s` '<task-name>'\n"
         "\t\tDelete task in first argument with value second argument\n\n"
-        "\t`-help` or `-h`\n"
+        "\t`--help` or `-h`\n"
         "\t\tShows man page for todolist\n"
     )
 
@@ -133,22 +144,22 @@ if __name__ == "__main__":
     action = sys.argv[1].lower()
 
     try:
-        if action == "-new" or action == "-n":
+        if action == "--new" or action == "-n":
             task_name = sys.argv[2]
             task = Task(task_name)
             new(task_name)
-        elif sys.argv[1] == "-update" or sys.argv[1] == "-u":
+        elif sys.argv[1] == "--update" or sys.argv[1] == "-u":
             task_name = sys.argv[2]
             new_name = sys.argv[3]
             update(task_name, new_name)
-        elif sys.argv[1] == "-delete" or sys.argv[1] == "-d":
+        elif sys.argv[1] == "--delete" or sys.argv[1] == "-d":
             task_name = sys.argv[2]
             delete(task_name)
-        elif sys.argv[1] == "-show" or sys.argv[1] == "-s":
+        elif sys.argv[1] == "--show" or sys.argv[1] == "-s":
             show()
-        elif sys.argv[1] == "-help" or sys.argv[1] == "-h":
+        elif sys.argv[1] == "--help" or sys.argv[1] == "-h":
             command_help()
         else:
-            print("Please refer to help (`-help` or `-h`) for instructions\n")
+            print("Please refer to help (`--help` or `-h`) for instructions\n")
     except IndexError as err:
-        print("Please refer to help (`-help` or `-h`) for instructions\n")
+        print("Please refer to help (`--help` or `-h`) for instructions\n")
